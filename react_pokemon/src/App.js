b770b8ef3c344e286a9b7644151a20a2acbb1d6d
catastroface <barna.urmossy@gmail.com>
2021-08-24 17:00:57 +0200

    Fetch data from API and assign it to state

--- react_pokemon/src/App.js
+++ react_pokemon/src/App.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import PokemonList from "./PokemonList";
 import TypeList from "./TypeList";
+import React from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const dataFromAPI = await fetchData();
+      setPokemon(dataFromAPI);
+    };
+
+    getData();
+  }, []);
+  const fetchData = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const data = res.json();
+    console.log(data);
+    return data;
+  };
+
   return (
     <div className="App">
       <Navbar />

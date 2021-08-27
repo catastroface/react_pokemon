cd931591b6bddfd74bc2617ce2514c0a3d92078d
catastroface <barna.urmossy@gmail.com>
2021-08-27 15:07:13 +0200

    Apply CSS to Typelist

--- react_pokemon/src/TypeList.js
+++ react_pokemon/src/TypeList.js
@@ -1,11 +1,17 @@
+import Tilty from "react-tilty";
+
 export default function TypeList({ types }) {
   return (
     <div className="content_window">
-      {types.map((type) => (
-        <div className="type">
-          <h3>{type.name}</h3>
-        </div>
-      ))}
+      <div class="types">
+        {types.map((type) => (
+          <Tilty perspective={1500} style={{ transformStyle: "preserve-3d" }}>
+            <div className={`type ${type.name}`}>
+              <h2>{type.name}</h2>
+            </div>
+          </Tilty>
+        ))}
+      </div>
     </div>
   );
 }
